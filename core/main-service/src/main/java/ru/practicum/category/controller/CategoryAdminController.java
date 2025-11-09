4919e7d45a50a9ab813ef5248a48678424d7f8a1
Слава <slavaspringerr@gmail.com>
2025-11-09 16:41:12 +0400

    remove ResponseEntity from CategoryAdminController

--- core/main-service/src/main/java/ru/practicum/category/controller/CategoryAdminController.java
+++ core/main-service/src/main/java/ru/practicum/category/controller/CategoryAdminController.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +21,8 @@ public class CategoryAdminController {
     private final CategoryAdminService categoryAdminService;
 
     @PostMapping
-    public ResponseEntity<CategoryDto> addCategory(
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDto addCategory(
             @RequestBody @Validated(CreateOrUpdateValidator.Create.class)
             CategoryDto requestCategory,
             BindingResult bindingResult
@@ -30,33 +30,24 @@ public class CategoryAdminController {
         log.info("Calling the POST request to /admin/categories endpoint");
         if (bindingResult.hasErrors()) {
             log.error("Validation error with category name");
-            return ResponseEntity.badRequest().body((requestCategory));
+            throw new IllegalArgumentException("Validation failed");
         }
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(categoryAdminService.createCategory(requestCategory));
+        return categoryAdminService.createCategory(requestCategory);
     }
 
     @DeleteMapping("/{catId}")
-    public ResponseEntity<String> deleteCategories(
-            @PathVariable @Positive Long catId
-    ) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategories(@PathVariable @Positive Long catId) {
         log.info("Calling the DELETE request to /admin/categories/{catId} endpoint");
         categoryAdminService.deleteCategory(catId);
-        return ResponseEntity
-                .status(HttpStatus.NO_CONTENT)
-                .body("Category deleted: " + catId);
     }
 
     @PatchMapping("/{catId}")
-    public ResponseEntity<CategoryDto> updateCategory(
+    public CategoryDto updateCategory(
             @PathVariable Long catId,
             @RequestBody @Validated(CreateOrUpdateValidator.Update.class) CategoryDto categoryDto
     ) {
         log.info("Calling the PATCH request to /admin/categories/{catId} endpoint");
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(categoryAdminService.updateCategory(catId, categoryDto));
+        return categoryAdminService.updateCategory(catId, categoryDto);
     }
-
 }
--- core/main-service/src/main/java/ru/practicum/event/mapper/EventMapper.java
+++ core/main-service/src/main/java/ru/practicum/event/mapper/EventMapper.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 
 public class EventMapper {
 
-
     public static Event toEvent(
             NewEventDto newEventDto,
             User initiator,
